432349f7fcbf4126df4175e963f7a8eca659fa55
Linukas <linukaszkr@gmail.com>
2019-05-19 11:59:41 +0200

    Initialt

---
+++ src/caculate.py
@@ -0,0 +1,89 @@
+import math
+import os
+import numpy as np
+
+from fractions import Fraction
+from decimal import Decimal
+from decimal import getcontext
+from sympy import *
+from numpy import *
+from sympy.abc import x, y
+
+# import sympy
+
+
+
+
+def science(n):
+    print("%e" % n)
+
+"------------------------------------------------------"
+
+# Constant definition
+h = 6.626e-34  # Planke Constant
+k = 1.38e-23  # Boltzman Constant
+m0 = 9.10938e-31  # Elektronen Mass
+eq = 1.60217648740e-19  # Elementarladung
+ev = 1.6021765314e-19  # ev to Joel
+E0 = 8.854187817e-12  # Dielektrizitaetkonstante
+
+pi = math.pi
+e = math.e
+
+def sqrt(n):
+    return float(math.sqrt(n))
+
+
+def ln(n):
+    return float(math.log(n))
+
+
+def dTf(self):
+    lenth = int(len(str(self)))
+    srrr = str(self)
+    indexOfDot = int(str(self).index('.'))
+    numberString = srrr[indexOfDot:lenth]
+    numberString = '0' + numberString
+    try:
+        f = float(numberString)
+    except ValueError:
+        print('invalid %s' % (numberString))
+    r = lenth - indexOfDot + 2
+    numerator = int(f * (10 ** r))
+    denominator = int(10 ** r)
+    result = Fraction(numerator, denominator)
+    return str(result)
+
+
+def getDerivative(f, n, time):
+    result = diff(f, n, time)
+    return result
+
+
+def getIntegrate(f, n):
+    result = str(integrate(f, n))
+    result += '+C'
+    return result
+dt=np.array([15,12,3,17,5,8])
+ma=6*756-60**2
+print(np.sum(dt[:]*dt[:]))
+print(ma)
+
+#############################################################
+#        while True:
+#           print('>>')
+#           dynamic = input('New Calculation: ')
+#           if dynamic != 'cls':
+#                   result=eval(dynamic)
+#                   print(str(result))
+#           else:
+#                   break
+#        os.system('cls')
+
+
+
+
+
+
+
+
